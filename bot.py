db3866247fa436773abde7fe6c89e9f0514612ca
unknown <codewithswastik@gmail.com>
2022-01-05 19:23:30 +0530

    Add support for users

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import os
+from typing import Union
+import discord
 from dotenv import load_dotenv
 from PIL import Image
 from discord.ext import commands
@@ -9,19 +11,21 @@ bot = commands.Bot(command_prefix="e>")
 
 
 @bot.command()
-async def emojify(ctx, url, size: int = 14):
+async def emojify(ctx, url: Union[discord.Member, str], size: int = 14):
+    if not isinstance(url, str):
+        url = url.display_avatar.url
+    
     def get_emojified_image():
         r = requests.get(url, stream=True)
         image = Image.open(r.raw).convert("RGB")
         res = emojify_image(image, size)
 
-        if size > 15:
+        if size > 14:
             res = f"```{res}```"
         return res
 
     result = await bot.loop.run_in_executor(None, get_emojified_image)
     await ctx.send(result)
-
 
 load_dotenv()
 bot.run(os.environ["TOKEN"])
