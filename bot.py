920f26c24bce8b5cc64a3a294d393bd4f87eabb5
Linus Torvalds <torvalds@linux-foundation.org>
2022-01-06 10:45:57 +0530

    Format

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from PIL import Image
 from discord.ext import commands
 from emojify import emojify_image
-import requests # It's not blocking, you can use aiohttp if you want
+import requests  # It's not blocking, you can use aiohttp if you want
 
 bot = commands.Bot(command_prefix="e>")
 
@@ -14,7 +14,7 @@ bot = commands.Bot(command_prefix="e>")
 async def emojify(ctx, url: Union[discord.Member, str], size: int = 14):
     if not isinstance(url, str):
         url = url.display_avatar.url
-    
+
     def get_emojified_image():
         r = requests.get(url, stream=True)
         image = Image.open(r.raw).convert("RGB")
@@ -26,6 +26,7 @@ async def emojify(ctx, url: Union[discord.Member, str], size: int = 14):
 
     result = await bot.loop.run_in_executor(None, get_emojified_image)
     await ctx.send(result)
+
 
 load_dotenv()
 bot.run(os.environ["TOKEN"])
